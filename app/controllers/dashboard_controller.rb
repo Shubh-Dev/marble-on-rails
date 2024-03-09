3f0f364b90b2184676463190082b1c0f98d1d6c3
Shubh <shubh.scb@gmail.com>
2024-03-09 22:17:50 +0530

    feat: basic authetication complete

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,9 @@
 
 # Controller for handling dashboard-related actions.
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
+
+  # GET /dashboard
   def index
     @user = current_user
   end

9398c2fadce041314f3e14747f1239e0b7e7dd73
Shubh <shubh.scb@gmail.com>
2024-03-10 08:56:40 +0530

    feat: sidebar in progress

---
+++ config/initializers/nav_items.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Path: config/initializers/nav_items.rb
+NAV_ITEMS = [
+  { id: 1, name: 'Dashboard', icon: 'fa-solid fa-house', path: '/' },
+  { id: 2, name: 'Customers', icon: 'fa-solid fa-user', path: '/customers' },
+  { id: 3, name: 'Sales', icon: 'fa-solid fa-circle-user', path: '/sales' },
+  { id: 4, name: 'Suppliers', icon: 'fa-regular fa-user', path: '/suppliers' },
+  { id: 5, name: 'Categories', icon: 'fa-solid fa-list', path: '/categories' },
+  { id: 6, name: 'Products', icon: 'fa-solid fa-box', path: '/products' },
+  { id: 7, name: 'Marbles', icon: 'fa-solid fa-diamond', path: '/marbles' },
+  { id: 8, name: 'Invoices', icon: 'fa-solid fa-file', path: '/invoices' },
+  { id: 9, name: 'Purchase Order', icon: 'fa-solid fa-bag-shopping', path: '/purchase-order' },
+  { id: 10, name: 'Subscriptions', icon: 'fa-solid fa-user-plus', path: '/subscriptions' },
+  { id: 11, name: 'Admin', icon: 'fa-solid fa-user-tie', path: '/admin' },
+  { id: 12, name: 'Logs', icon: 'fa-solid fa-wave-square', path: '/logs' },
+  { id: 3, name: 'Settings', icon: 'fa-solid fa-gear', path: '/settings' }
+].freeze
